451b169482d1d6fb060e308055a315d4fd8a2687
Sylvain T <garvek@gmail.com>
2021-06-23 17:29:19 +0200

    Update dependencies Switch from scss-lint to stylelint Update version Prepare for delivery

--- rpg-awesome-tests.js
+++ rpg-awesome-tests.js
@@ -17,7 +17,7 @@ var fontAssets = [
 // Check that the font files are downloadable. Meteor places assets at /packages/<packageName>/.
 fontAssets.forEach(function (filename) {
     Tinytest.addAsync(filename + ' font is shipped', function (test, done) {
-      var path = '/packages/nagoshiashumari_rpg-awesome/fonts/' + filename;
+      var path = '/packages/garvek_rpg-awesome/fonts/' + filename;
       HTTP.get(path, function callback(error, result) {
         if (error) {
           test.fail({message: 'Font failed to load'});
@@ -44,7 +44,7 @@ Tinytest.addAsync('Visual check', function (test, done) {
 
   // TODO ideally we'd get src/test.html straight from this repo, but no idea
   //      how to do this from TinyTest
-  HTTP.get('https://rawgit.com/nagoshiashumari/Rpg-Awesome/gh-pages/index.html', function callback(error, result) {
+  HTTP.get('https://rawgit.com/garvek/Rpg-Awesome/gh-pages/index.html', function callback(error, result) {
     if (error) {
       test.fail('Error getting the icons. Do we have an Internet connection to rawgit.com?');
     } else {
